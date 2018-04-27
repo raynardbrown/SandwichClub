2a87353377ffb89b3b3f0cfacf18dc5a208dd0c0
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-04-27 17:52:58 -0500

    Changed the populateUI function to take a Sandwich parameter.
    Added two helper functions populateTextViewWithList and
    populateTextViewWithString.
    
    Added missing return statement in intent null check to fix null pointer
    exception warning.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView alsoKnownAsTextView;
+    private TextView placeOfOriginTextView;
+    private TextView descriptionTextView;
+    private TextView ingredientsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +29,15 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        alsoKnownAsTextView = findViewById(R.id.tv_also_known_as);
+        placeOfOriginTextView = findViewById(R.id.tv_place_of_origin);
+        descriptionTextView = findViewById(R.id.tv_description);
+        ingredientsTextView = findViewById(R.id.tv_ingredients);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
+            return;
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
@@ -43,7 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +69,71 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     * Populate the non-label text views within this activity with the values from the specified
+     * sandwich model.
+     *
+     * @param sandwich the sandwich model used to populate all the non-label textviews within this
+     *                 activity.
+     */
+    private void populateUI(Sandwich sandwich) {
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        DetailActivity.populateTextViewWithList(alsoKnownAsTextView, alsoKnownAsList);
 
+        populateTextViewWithString(placeOfOriginTextView, sandwich.getPlaceOfOrigin());
+
+        populateTextViewWithString(descriptionTextView, sandwich.getDescription());
+
+        List<String> ingredientsList = sandwich.getIngredients();
+        DetailActivity.populateTextViewWithList(ingredientsTextView, ingredientsList);
+    }
+
+    /**
+     * Helper function used to populate the specified text view with the all the strings from the
+     * specified list. The string shall be displayed within the specified text view, comma
+     * separated. If the specified list is empty the text view is populated with the string "N/A".
+     *
+     * @param textView the text view that will be populated with the strings from the specified list.
+     * @param list the list containing the strings that will be populated within the specified text view.
+     */
+    private static void populateTextViewWithList(TextView textView, List<String> list)
+    {
+        if(list.isEmpty())
+        {
+            textView.setText("N/A");
+        }
+        else
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(list.get(0));
+
+            for(int i = 1; i < list.size(); ++i)
+            {
+                sb.append(", ");
+                sb.append(list.get(i));
+            }
+
+            textView.setText(sb.toString());
+        }
+    }
+
+    /**
+     * Helper function used to populate the specified text view with the specified string. If the
+     * specified string is empty, then the text view is populated with the string "N/A".
+     *
+     * @param textView the text view that will be populated with the specified string.
+     * @param string the string that will be populated within the specified text view.
+     */
+    private static void populateTextViewWithString(TextView textView, String string)
+    {
+        if(string.length() > 0)
+        {
+            textView.setText(string);
+        }
+        else
+        {
+            textView.setText("N/A");
+        }
     }
 }
